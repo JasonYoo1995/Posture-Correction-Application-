8d147f6b66524ba3368bb5e0acb4a198a8086706
JasonYoo1995 <jasonyoo950909@gmail.com>
2020-09-20 23:36:18 +0900

    server and DB connected for login/logout/signup

--- app/src/main/java/com/example/bottomtab/LoginActivity.java
+++ app/src/main/java/com/example/bottomtab/LoginActivity.java
@@ -1,16 +1,36 @@
 package com.example.bottomtab;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class LoginActivity extends AppCompatActivity {
+    EditText IDText, PWText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+
+        IDText = findViewById(R.id.sign_in_id);
+        PWText = findViewById(R.id.sign_in_pw);
 
         findViewById(R.id.layout_sign_up_btn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,15 +42,101 @@ public class LoginActivity extends AppCompatActivity {
 
         findViewById(R.id.layout_sign_in_btn).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { // 계정 생성 버튼 누르면 다음 화면
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            public void onClick(View v) {
+                JSONTask task = new JSONTask();
+                Data data[] = {new Data("id", IDText.getText().toString()), new Data("pw", PWText.getText().toString())};
+                task.setData(data);
+                task.execute("http://3.92.215.113:4001/sign_in");//AsyncTask 시작시킴
             }
         });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
+    }
+
+    private void makeToast(String string){
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+        Data[] data;
+        public void setData(Data[] data){
+            this.data = data;
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                for (int i=0; i<data.length; i++){
+                    jsonObject.accumulate(data[i].key, data[i].value);
+                }
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+                try{
+                    //URL url = new URL("http://3.92.215.113:4001/users");
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(result.equals("-1")){
+                makeToast("회원 정보가 불일치합니다");
+            }
+            else{
+                makeToast("로그인 되었습니다");
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/bottomtab/SignUpActivity.java
+++ app/src/main/java/com/example/bottomtab/SignUpActivity.java
@@ -1,20 +1,150 @@
 package com.example.bottomtab;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class SignUpActivity extends AppCompatActivity {
+    EditText IDText, PWText, nameText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_sign_up);
 
-        findViewById(R.id.create_account_btn).setOnClickListener(new View.OnClickListener() {
+        IDText = findViewById(R.id.sign_up_id);
+        PWText = findViewById(R.id.sign_up_pw);
+        nameText = findViewById(R.id.sign_up_name);
+
+        findViewById(R.id.sign_up_cancel_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        findViewById(R.id.create_account_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(IDText.getText().toString().equals("")){
+                    makeToast("아이디를 입력하세요");
+                    return;
+                }
+                else if(PWText.getText().toString().equals("")){
+                    makeToast("비밀번호를 입력하세요");
+                    return;
+                }
+                else if(nameText.getText().toString().equals("")){
+                    makeToast("이름을 입력하세요");
+                    return;
+                }
+
+                JSONTask task = new JSONTask();
+                Data data[] = {new Data("id", IDText.getText().toString()),
+                        new Data("pw", PWText.getText().toString()),
+                        new Data("name", nameText.getText().toString())};
+                task.setData(data);
+                task.execute("http://3.92.215.113:4001/sign_up");//AsyncTask 시작시킴
+            }
+        });
+    }
+
+    private void makeToast(String string){
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT).show();
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+        Data[] data;
+        public void setData(Data[] data){
+            this.data = data;
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                for (int i=0; i<data.length; i++){
+                    jsonObject.accumulate(data[i].key, data[i].value);
+                }
+                HttpURLConnection con = null;
+                BufferedReader reader = null;
+                try{
+                    //URL url = new URL("http://3.92.215.113:4001/users");
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if(result.equals("-1")){
+                makeToast("아이디가 중복됩니다");
+            }
+            else{
+                makeToast("회원 가입이 되셨습니다");
+                finish();
+            }
+        }
     }
 }
+
